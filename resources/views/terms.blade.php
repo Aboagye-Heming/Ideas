6c729376d03fb0644c249bfa18f5fb11c0fcbb2e
Aboagye=Heming <hemingkwabena@gmail.com>
2023-10-30 14:56:44 +0000

    added terms page

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+
+@section('content')
+<h1>Terms</h1>
+<div>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi natus consequatur aliquid dolorem
+    voluptatem ad, maiores amet adipisci ex! Voluptas, iusto dolor excepturi eius voluptatum aut error sapiente
+    at exercitationem, sint magnam assumenda veritatis. Et velit ipsa mollitia ipsam, temporibus optio laborum
+    porro, nam, praesentium voluptatum facilis vero aperiam commodi! Accusamus officia cupiditate sit at!
+</div>
+@endsection
